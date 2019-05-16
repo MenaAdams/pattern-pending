2d43c19d4f951247eb7aa3eed79b13f8dbbf604c
Mena Adams <menaadams@gmail.com>
2019-05-16 15:28:57 -0700

    added get user data route

--- server.py
+++ server.py
@@ -15,9 +15,37 @@ RAVELRY_PASSWORD = os.environ.get('RAVELRY_PASSWORD')
 
 @app.route("/")
 def homepage():
+    """ Render homepage. """
 
     return render_template('homepage.html')
 
+@app.route("/rav-data")
+def get_users_rav_data():
+    """ Grab user's personal ravelry data"""
+    username = request.args.get('username')
+
+    if api.user_is_valid(username):
+        user_patts = {}
+        user_data_functions = [api.get_user_favs(username),
+                            api.get_user_queue(username),
+                            api.get_user_library(username)]
+        #loop through patterns in users favs, queue, library
+        #count how many times pattern appears
+        for funct in user_data_functions:
+            patts = funct
+            print(patts)
+            for patt in patts:
+                if user_patts.get('patt'):
+                    user_patts[patt] += 1
+                else:
+                    user_patts[patt] = 1
+        print(user_patts, "=========user_patts==========")
+        return render_template('user.html',
+                                # user_patts = user_patts,
+                                )
+    else:
+        flash(f'"{username}" is not a valid ravelry account. Please try again.')
+        return redirect('/')
 
 
 if __name__ == "__main__":
