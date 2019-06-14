4cc4c068ca34afc9f835a1e4bc46cf1f2e35f5d4
Mena Adams <menaadams@gmail.com>
2019-06-14 07:25:16 -0700

    changed loading gif again and fixed homepage text input field

--- static/homepage.js
+++ static/homepage.js
@@ -1,7 +1,7 @@
 const form = `
 <form action="/rav-data" method="POST">
   <label for="username">Ravelry Username:</label>
-  <input type="text" name="username" class="text" id="username" placeholder="Username">
+  <input type="text" name="username" class="text" id="username" placeholder="Username" required>
   <input type="submit" class="submit-button">
 </form>`
 
